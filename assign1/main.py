257e7917db9d5a1c383dff9db1a33091d520541c
Lei Hsiung <nthu.email@gmail.com>
2021-03-28 21:17:04 +0800

    fix bugs & finish the report

--- assign1/main.py
+++ assign1/main.py
@@ -1,9 +1,10 @@
 import pydicom
 import numpy as np
 import matplotlib.pyplot as plt
-import skimage
 from math import sqrt
+import time
 import os
+from vedo import *
 
 dataset_dir = '../CT_chest_scans/'
 result_img_dir = 'CT_chest_scans_segmentation/'
@@ -16,9 +17,7 @@ def read_by_patient_id(patient_id):
     return patient_dcms
 
 
-def print_data_fields(dsm_img):
-    ds = pydicom.dcmread(dsm_img)
-
+def print_data_fields(ds):
     # get the pixel information into a numpy array
     data = ds.pixel_array
     print('The image has {} x {} voxels'.format(data.shape[0],
@@ -36,24 +35,73 @@ def print_data_fields(dsm_img):
     print('The information of the data set after downsampling: \n')
     print(ds)
 
-def extract_raw_and_hounsfield (dcm_img):
-    ds = pydicom.dcmread(dcm_img)
-    rSlope = ds.RescaleSlope
-    rIntercept = ds.RescaleIntercept
-    instanceNum = ds.InstanceNumber
-    img_height, img_width = ds.pixel_array.shape
-    dcm_img_pixel_array = np.clip(ds.pixel_array, 0, None)
 
-    dcm_img_hu = np.zeros((img_height, img_width))
-    for i in range(img_height):
-        for j in range(img_width):
-            dcm_img_hu[i][j] = dcm_img_pixel_array[i][j]*rSlope + rIntercept
-    return instanceNum, dcm_img_pixel_array, dcm_img_hu
+def compute_statistic(img):
+    flatten_img = img.flatten()
+    zero_idx = flatten_img==flatten_img.min()
+    res = flatten_img[~zero_idx]
+
+    return res.max(), res.min(), res.mean(), res.std()
+
+def read_dicom_to_list (patient_dicoms):
+    st = time.time()
+    dicom_list = list()
+    dicom_num = 0
+    dicom_reject = 0
+    for file_name in patient_dicoms:
+        ds = pydicom.dcmread(file_name)
+        try:
+            if hasattr(ds, 'SliceLocation'):
+                dicom_list.append(ds)
+                dicom_num += 1
+            else:
+                dicom_reject += 1
+                print("reject: ", file_name)
+        except Exception as e:
+            pass
+
+    print("read done %s s | %d files | %d rejected" % (round(time.time() - st,2), dicom_num, dicom_reject))
+
+    if len(dicom_list) <= 0:
+        return False
+    dicom_list.sort(key=lambda x: x.InstanceNumber)
+
+    return dicom_list
 
 
-def thresholding(img):
-    threshold = 0.24 #img.median()
+def get_pixel_array_from_dicom (dicom_list):
+    dcm_img_raw_list = list()
+    dcm_img_hu_list = list()
 
+    for dicom_file in dicom_list:
+        rSlope = dicom_file.RescaleSlope
+        rIntercept = dicom_file.RescaleIntercept
+        pix_spacing = dicom_file.PixelSpacing
+        img_height, img_width = dicom_file.pixel_array.shape
+
+        dcm_img_raw = np.clip(dicom_file.pixel_array.astype(np.float32), 0, None)
+        dcm_img_hu = dcm_img_raw*rSlope + rIntercept
+
+        dcm_img_raw_list.append(dcm_img_raw)
+        dcm_img_hu_list.append(dcm_img_hu)
+
+    return dcm_img_raw_list, dcm_img_hu_list
+
+def median_threshold(img):
+    flatten_img = img.flatten()
+    zero_idx = flatten_img==0
+    removed_zero = flatten_img[~zero_idx]
+    median = np.median(removed_zero)
+    return median
+
+def mean_threshold(img):
+    flatten_img = img.flatten()
+    zero_idx = flatten_img==0
+    removed_zero = flatten_img[~zero_idx]
+    mean = removed_zero.mean()
+    return mean
+
+def thresholding(img, threshold):
     white = img>=threshold
     black = img<threshold
     img[white] = 1.
@@ -61,7 +109,7 @@ def thresholding(img):
     return img
 
 
-def img_nomarlize(img):
+def img_normalize(img):
     img_max = img.max()
     img_min = img.min()
 
@@ -69,7 +117,7 @@ def img_nomarlize(img):
 
     return normalized_img
 
-def print_list_of_images(img_list, patient_id):
+def print_list_of_images(img_list, patient_id, apply_threshold=True, save_img=False):
     h, w = img_list[0].shape
     fig = plt.figure(figsize=(10, 10))
     columns = rows = int(sqrt(len(img_list)))
@@ -80,22 +128,81 @@ def print_list_of_images(img_list, patient_id):
     for i in range(columns * rows):
         ax.append(fig.add_subplot(rows, columns, i + 1))
         ax[-1].set_title("slice:" + str(i))  # set title
-        plt.imshow(thresholding(img_nomarlize(img_list[i])), cmap='gray')
+        normal_img = img_normalize(img_list[i])
+        if apply_threshold:
+            threshold = mean_threshold(normal_img)
+            plt.imshow(thresholding(normal_img, threshold), cmap='gray')
+        else:
+            plt.imshow(normal_img, cmap='gray')
         plt.axis('off')
 
 
     plt.show()  # finally, render the plot
+    if save_img:
+        if not os.path.exists(result_img_dir):
+            os.mkdir(result_img_dir)
+        fig.savefig(result_img_dir + 'patient_' + format(patient_id, '02d'))
+
+
+def print_one_image(img, img_name):
+    h, w = img.shape
+    fig = plt.figure(figsize=(8, 8))
+
+    plt.imshow(img, cmap='gray')
+    plt.axis('off')
+
+    plt.show()  # finally, render the plot
     if not os.path.exists(result_img_dir):
         os.mkdir(result_img_dir)
-    fig.savefig(result_img_dir + 'patient_' + str(patient_id))
+    fig.savefig(result_img_dir + img_name)
 
-
-def show_hist(img_pixel):
-    plt.hist(img_pixel.flatten(), bins=50, color='c')
+def show_hist(img_pixel, threshold_value=None):
+    # remove value 0
+    flatten_img = img_pixel.flatten()
+    zero_idx = flatten_img==0
+    plt.hist(flatten_img[~zero_idx], bins=50, color='c')
+    if threshold_value:
+        plt.axvspan(threshold_value, threshold_value+0.001, color='red', alpha=0.5)
     plt.title('Normalized Image')
     # plt.xlabel("Hounsfield Units (HU)")
     # plt.ylabel("Frequency")
-    plt.show()
+    # plt.show()
+
+def flip_black_and_white(imgs):
+    white = (imgs == 1.)
+    new_img = imgs
+    new_img[white] = 0.
+    new_img[~white] = 1.
+    return new_img
+
+def draw_in_3D (dicom_files, hu_imgs):
+    unset = True
+    hu_stack = None
+    pix_spacing = None
+    z_spacing = None
+    segmentation_imgs = [thresholding(img_normalize(hu_img), mean_threshold(hu_img)) for hu_img in hu_imgs]
+    for i in range(len(dicom_files)):
+        arr = flip_black_and_white(segmentation_imgs[i])*255
+
+        if unset:
+            imShape = (arr.shape[0], arr.shape[1], len(dicom_files))
+            hu_stack = np.zeros(imShape)
+            pix_spacing = dicom_files[i].PixelSpacing
+            dist = 0
+            for j in range(2):
+                cs = [float(q) for q in dicom_files[j].ImageOrientationPatient]
+                ipp = [float(q) for q in dicom_files[j].ImagePositionPatient]
+                parity = pow(-1, j)
+                dist += parity*(cs[1]*cs[5] - cs[2]*cs[4])*ipp[0]
+                dist += parity*(cs[2]*cs[3] - cs[0]*cs[5])*ipp[1]
+                dist += parity*(cs[0]*cs[4] - cs[1]*cs[3])*ipp[2]
+            z_spacing = abs(dist)
+            unset = False
+        hu_stack[:, :, i] = arr
+
+    pix_spacing.append(z_spacing)
+    vol = Volume(hu_stack, spacing=pix_spacing)
+    show(vol)
 
 
 if __name__ == '__main__':
@@ -104,34 +211,21 @@ if __name__ == '__main__':
 
     for i in range(len(patient_ids)):
         patient_cts = read_by_patient_id(patient_ids[i])
+        patient_dicoms = read_dicom_to_list(patient_cts)
 
-        hu_imgs = list()
-        hu_imgs_id = list()
-        cnt = 0
+        # Read in and print out all the data fields in a DICOM file
+        # print_data_fields(patient_dicoms[0])
 
-        for patient_ct in patient_cts:
-            # Read in and print out all the data fields in a DICOM file
-            # print(patient_ct)
-            # print_data_fields(patient_ct)
+        raw_imgs, hu_imgs = get_pixel_array_from_dicom(patient_dicoms)
 
-            patient_ct_id, patient_ct_raw, patient_ct_hu = extract_raw_and_hounsfield (patient_ct)
+        # Read in the raw data for a CT slice and convert its pixel values into Hounsfield units.
+        # Compute the max, min, mean and standard deviation of both images (raw data and Hounsfield units).
+        # raw_max, raw_min, raw_mean, raw_std = compute_statistic(raw_imgs[0])
+        # hu_max, hu_min, hu_mean, hu_std = compute_statistic(hu_imgs[0])
+        #
+        # print(f'raw_max: {raw_max}, raw_min: {raw_min}, raw_mean: {raw_mean}, raw_std: {raw_std}')
+        # print(f'hu_max: {hu_max}, hu_min: {hu_min}, hu_mean: {hu_mean}, hu_std: {hu_std}')
 
-            # Read in the raw data for a CT slice and convert its pixel values into Hounsfield units.
-            # Compute the max, min, mean and standard deviation of both images (raw data and Hounsfield units).
-            # raw_max, raw_min, raw_mean, raw_std = (patient_ct_raw.max(), patient_ct_raw.min(), patient_ct_raw.mean(), patient_ct_raw.std())
-            # hu_max, hu_min, hu_mean, hu_std = (patient_ct_hu.max(), patient_ct_hu.min(), patient_ct_hu.mean(), patient_ct_hu.std())
-            #
-            # print(f'raw_max: {raw_max}, raw_min: {raw_min}, raw_mean: {raw_mean}, raw_std: {raw_std}')
-            # print(f'hu_max: {hu_max}, hu_min: {hu_min}, hu_mean: {hu_mean}, hu_std: {hu_std}')
-
-            # show_hist(normal_img)
-
-            hu_imgs.append(patient_ct_hu)
-            hu_imgs_id.append(patient_ct_id)
-            cnt = cnt + 1
-            if cnt == 25:
-                break
-
-        hu_imgs = [x for _,x in sorted(zip(hu_imgs_id,hu_imgs))]
-        print_list_of_images(hu_imgs, i)
-        break
+        print_list_of_images(hu_imgs[:25], i, apply_threshold=False, save_img=False)
+        # draw_in_3D(patient_dicoms, hu_imgs)
+        break # demo only one patient
